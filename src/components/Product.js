da99b38ca2f446ebb023cc3adc760dd080284f2f
louis baptiste <louisvis@ymail.com>
2020-06-21 16:10:40 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,32 +1,45 @@
-// Code Product Component Here
-class Order extends React.Component {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Product extends React.Component {
 
   render() {
     return (
-      <div className="order">
-        <ul>
-          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
-          <li>{this.props.size}</li>
-          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
-          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
-        </ul>
-      </div>
-    );
+      <div>Product</div>
+    )
   }
 }
 
-Order.defaultProps = {
-  cone: true,
-  size: 'regular'
-};
+Product.defaultProps = {
+  hasWatermark: false
+}
 
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf([
+    'white',
+    'eggshell-white',
+    'salmon'
+  ]).isRequired,
+  weight: (props, propName) => {
+    const weight = props[propName];
 
-Order.propTypes = {
- cone: PropTypes.bool,
- size: PropTypes.string,
- scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
- orderInfo: PropTypes.shape({
-   customerName: PropTypes.string.isRequired,
-   orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
- }).isRequired
-};
+    if (weight === undefined) {
+      return new Error('The `weight` prop is required.');
+    }
+
+    if (isNaN(weight)) {
+      return new Error('The `weight` prop is not a number.');
+    }
+
+    const isValidWeight = weight > 80 && weight < 300;
+
+    if (!isValidWeight) {
+      return new Error('The `weight` prop should range between 80 and 300.');
+    }
+  },
+}
+
+export default Product;
